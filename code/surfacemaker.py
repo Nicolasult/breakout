ef74efb082f7ea39fcb675694e39fc9ea2796856
nicolasult <nicolas.ultrillo@gmail.com>
2025-07-12 12:07:27 +0200

    structure path to graphics in the surfacemaker class

--- code/surfacemaker.py
+++ code/surfacemaker.py
@@ -1,7 +1,18 @@
 import pygame
 from settings import *
+from os import walk
 
 class SurfaceMaker:
+    def __init__(self):
+        for index, info in enumerate(walk("graphics/blocks")):
+            if index == 0:
+                self.assets = {color:{} for color in info[1]}
+            else:
+                for image_name in info[2]:
+                    color_type = list(self.assets.keys())[index - 1]
+                    full_path = "graphics/blocks" + f"{color_type}/" + image_name
+                    print(full_path)
+
     def get_surf(self, block_type, size):
         image = pygame.Surface(size)
         image.fill("red")
