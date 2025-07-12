ac87ebcbe69b5b794b8f9859a68fb1ed8b3cbeb8
nicolasult <nicolas.ultrillo@gmail.com>
2025-07-12 13:08:51 +0200

    upgrade class setup

--- code/sprites.py
+++ code/sprites.py
@@ -2,6 +2,23 @@ import pygame
 from settings import *
 from random import choice
 
+class Upgrade(pygame.sprite.Sprite):
+    def __init__(self, , pos, upgrade_type, groups):
+        super().__init__(groups)
+        self.upgrade_type = upgrade_type
+        self.image = pygame.image.load(f"graphics/upgrades.{upgrade_type}").convert_alpha()
+        self.rect = self.image.get_rect(midtop = pos)
+        
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.speed = 300
+
+    def update(self, dt):
+        self.pos.y += self.speed * dt
+        self.rect.y = round(self.pos.y)
+
+        if self.rect.top > WINDOW_HEIGHT + 100:
+            self.kill()
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, groups, surfacemaker):
         super().__init__(groups)
